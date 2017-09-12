c0beca4773d41b0fe9ba14303688892c6183f382
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-09-12 23:48:35 +0900

    [section 6.5] Add "add" method

--- src/main/scala/chapter06/Appli.scala
+++ src/main/scala/chapter06/Appli.scala
@@ -13,4 +13,11 @@ object Appli extends App {
   // 事前条件によって例外IllegalArgumentExceptionが発生する
   // val wrongRational = new Rational(5, 0)
   // println("wrongRational: Rational = " + wrongRational)
+
+  val res = oneHalf add twoThirds
+  println(s"1/2 + 2/3 = $res")
+
+  val r = new Rational(1, 2)
+  println(s"r.numer = ${r.numer}")
+  println(s"r.denom = ${r.denom}")
 }
--- src/main/scala/chapter06/Rational.scala
+++ src/main/scala/chapter06/Rational.scala
@@ -2,6 +2,11 @@ package chapter06
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
+  val numer: Int = n
+  val denom: Int = d
 
-  override def toString = n + "/" + d
+  override def toString = numer + "/" + denom
+
+  def add(that: Rational): Rational =
+    new Rational(numer * that.denom + that.numer * denom, denom * that.denom)
 }
