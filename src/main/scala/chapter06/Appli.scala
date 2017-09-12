a2f22e35e33d5a693f4abfd33258f05226619e5e
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-09-12 23:49:43 +0900

    Fix to use string interpolation

--- src/main/scala/chapter06/Appli.scala
+++ src/main/scala/chapter06/Appli.scala
@@ -4,8 +4,8 @@ object Appli extends App {
   val oneHalf = new Rational(1, 2)
   val twoThirds = new Rational(2, 3)
 
-  println("oneHalf: Rational = " + oneHalf)
-  println("twoThirds: Rational = " + twoThirds)
+  println(s"oneHalf: Rational = $oneHalf")
+  println(s"twoThirds: Rational = $twoThirds")
 
   // 演算子を定義していないのでエラーになる
   // (oneHalf / 7) + (1 - twoThirds)
