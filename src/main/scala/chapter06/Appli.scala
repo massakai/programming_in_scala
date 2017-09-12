9a996358c8864d4a54ed2f5503dd038c5bd0b2b3
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-09-12 23:32:04 +0900

    [section 6.3] Add toString method

--- src/main/scala/chapter06/Appli.scala
+++ src/main/scala/chapter06/Appli.scala
@@ -4,6 +4,9 @@ object Appli extends App {
   val oneHalf = new Rational(1, 2)
   val twoThirds = new Rational(2, 3)
 
+  println("oneHalf: Rational = " + oneHalf)
+  println("twoThirds: Rational = " + twoThirds)
+
   // 演算子を定義していないのでエラーになる
   // (oneHalf / 7) + (1 - twoThirds)
 }
--- src/main/scala/chapter06/Rational.scala
+++ src/main/scala/chapter06/Rational.scala
@@ -1,5 +1,5 @@
 package chapter06
 
 class Rational(n: Int, d: Int) {
-  println("Created " + n + "/" + d)
+  override def toString = n + "/" + d
 }
