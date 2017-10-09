54f2e07ddb667cd5bd38f621012ca44ae0e0d917
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-10-10 07:06:18 +0900

    Add section 8.1

---
+++ src/main/scala/chapter08/FindLongLines.scala
@@ -0,0 +1,9 @@
+package chapter08
+
+object FindLongLines {
+  def main(args: Array[String]): Unit = {
+    val width = args(0).toInt
+    for (arg <- args.drop(1))
+      LongLines.processFile(arg, width)
+  }
+}
