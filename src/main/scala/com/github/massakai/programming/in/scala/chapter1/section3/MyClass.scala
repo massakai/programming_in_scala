3590ae3ff440cb624a9742c7409f1c4f9ee1d533
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-08-05 15:59:44 +0900

    フィールドの値を確認するため、valを追加

--- src/main/scala/com/github/massakai/programming/in/scala/chapter1/section3/MyClass.scala
+++ src/main/scala/com/github/massakai/programming/in/scala/chapter1/section3/MyClass.scala
@@ -1,3 +1,3 @@
 package com.github.massakai.programming.in.scala.chapter1.section3
 
-class MyClass(index: Int, name: String)
+class MyClass(val index: Int, val name: String)
--- src/test/scala/com/github/massakai/programming/in/scala/chapter1/section3/MyClassSpec.scala
+++ src/test/scala/com/github/massakai/programming/in/scala/chapter1/section3/MyClassSpec.scala
@@ -6,9 +6,9 @@ import org.scalatest.matchers.should
 class MyClassSpec extends AnyFlatSpec with should.Matchers {
   "MyClass(30, \"hoge\")" should "have index 30 and name\"hoge\"" in {
     val myClass = new MyClass(30, "hoge")
-    // index, nameにアクセスできない
-    // myClass.index shouldEqual 30
-    // myClass.name shouldEqual "hoge"
+
+    myClass.index shouldEqual 30
+    myClass.name shouldEqual "hoge"
   }
 }
 
