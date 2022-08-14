edafbe1cc721c5ad02cfdce8e11a66e71324efcf
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-08-14 22:05:58 +0900

    sbtのビルドが通るように修正

---
+++ src/main/scala/com/github/massakai/programming/in/scala/chapter2/Hello.scala
@@ -0,0 +1,5 @@
+package com.github.massakai.programming.in.scala.chapter2
+
+object Hello extends App {
+  println("Hello, world, from a script!")
+}
---
+++ src/main/scala/com/github/massakai/programming/in/scala/chapter2/HelloArg.scala
@@ -0,0 +1,5 @@
+package com.github.massakai.programming.in.scala.chapter2
+
+object HelloArg extends App {
+  println("Hello, " + args(0) + "!")
+}
